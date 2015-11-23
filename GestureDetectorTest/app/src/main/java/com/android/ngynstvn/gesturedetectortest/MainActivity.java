8b9a437e5e986dba74c0592f6cbc80f3638a8aa3
Steven Nguyen <ngynstvn@gmail.com>
2015-11-22 22:45:22 -0800

    Added dummy image to the application. It's Waldo time...

--- GestureDetectorTest/app/src/main/java/com/android/ngynstvn/gesturedetectortest/MainActivity.java
+++ GestureDetectorTest/app/src/main/java/com/android/ngynstvn/gesturedetectortest/MainActivity.java
@@ -3,16 +3,23 @@ package com.android.ngynstvn.gesturedetectortest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private ImageView testImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.tb_activity_main);
+        testImage = (ImageView) findViewById(R.id.iv_test_image);
         setSupportActionBar(toolbar);
+
+        Picasso.with(getApplicationContext()).load(R.drawable.find_waldo).into(testImage);
     }
 }
