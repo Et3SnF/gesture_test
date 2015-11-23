f48b8d1b16daf6ecf90ffdc8c8feede2d0b85a6f
Steven Nguyen <ngynstvn@gmail.com>
2015-11-22 22:20:00 -0800

    Initial commit of scratch work

---
+++ GestureDetectorTest/app/src/main/java/com/android/ngynstvn/gesturedetectortest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.android.ngynstvn.gesturedetectortest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.tb_activity_main);
+        setSupportActionBar(toolbar);
+    }
+}
